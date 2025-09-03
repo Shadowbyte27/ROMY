8e22d74b3ad0a4c91d03850718e4bfcc6f300b3f
ROMEO <empereurayakawa@gmail.com>
2025-09-03 04:12:14 +0200

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61563805239136"];
+		const permission = ["61578956837490","61563805239136"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("You don't have permission to use this command.", event.threadID, event.messageID);
 		}
